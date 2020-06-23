15ced9cb2bca8aadcc2397db04d7f8220a2d4ac8
nlukic97 <nlukic97@gmail.com>
2020-06-23 22:06:15 +0200

    socket.io making, work in progress

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-var express = require('express');
-var Ws = require('ws');
-var app = express();
+var app = require('express')();
+var http = require('http').createServer(app)
+var io = require('socket.io')(http);
 var port = 3000;
 
-app.listen(port,function(){
-  console.log('Server started at port ' + port + '.');
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname + '/public/index.html');
 })
 
-app.use(express.static('./public'));
+app.get('/client2/client.html',(req,res)=>{
+  res.sendFile(__dirname + '/public/client2/client.html');
+})
 
-var server = new Ws.Server({port:'3200'}); //mora veliko S
+app.get('/client3/client3.html',(req,res)=>{
+  res.sendFile(__dirname + '/public/client3/client3.html');
+})
 
-server.on('connection',function(client){
-  console.log('new user arived to server.');
-
-  client.on('close',function(){
-    console.log('Client has left the server.');
+io.on('connection',(socket)=>{
+  console.log('a user has connected')
+  socket.on('disconnect',()=>{
+    console.log('user has disconected.')
+  })
+  socket.on('end',()=>{
+    socket.disconnect(0)
   })
 
-  client.on('message',function(msg){
-    console.log('Client says: ' + msg);
-    client.send(msg);
+  socket.on('message',(msg)=>{
+    console.log('the User has said: ' + msg)
+
+    console.log('Sending the user: ' + msg + '...')
+    io.emit('message',msg)
   })
 })
+
+http.listen(3000,()=>{
+  console.log('listening on port 3000.')
+})
+
+//page index.html is fully socket.io optimised. Other two pages are introduced in the server, but still have remaining vue methods, and function calls which need to be replaced.
 
 
 
