6bbd56bfc420cbaf4914e298196776dc7a1fbc3a
nlukic97 <nlukic97@gmail.com>
2020-06-25 15:59:36 +0200

    fix ?

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-var app = require('express')();
+var express = require('express') //moras odvojeno express
+var app = express();
+
 var http = require('http').createServer(app)
 var io = require('socket.io')(http);
 var port = 3000;
 
-app.get('/',(req,res)=>{
-  res.sendFile(__dirname + '/public/index.html');
-})
+// app.get('/',(req,res)=>{
+//   res.sendFile(__dirname + '/public/index.html');
+// })
 
-app.get('/client2/client.html',(req,res)=>{
-  res.sendFile(__dirname + '/public/client2/client.html');
-})
+// app.get('/client2/client.html',(req,res)=>{
+//   res.sendFile(__dirname + '/public/client2/client.html');
+// })
 
-app.get('/client3/client3.html',(req,res)=>{
-  res.sendFile(__dirname + '/public/client3/client3.html');
-})
+// app.get('/client3/client3.html',(req,res)=>{
+//   res.sendFile(__dirname + '/public/client3/client3.html');
+// })
+
+app.use(express.static('./public'));
 
 io.on('connection',(socket)=>{
   console.log('a user has connected')
@@ -29,11 +33,11 @@ io.on('connection',(socket)=>{
     console.log('the User has said: ' + msg)
 
     console.log('Sending the user: ' + msg + '...')
-    io.emit('message',msg)
+    socket.emit('message1',msg)
   })
 })
 
-http.listen(3000,()=>{
+http.listen(port,()=>{
   console.log('listening on port 3000.')
 })
 
